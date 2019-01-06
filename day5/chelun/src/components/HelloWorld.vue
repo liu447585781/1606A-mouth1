a738fa8d5c417c18bc5fbbf466f843ea35f02352
liu447585781 <447585781@qq.com>
2019-01-06 21:25:24 +0800

    change twelve

--- day5/chelun/src/components/HelloWorld.vue
+++ day5/chelun/src/components/HelloWorld.vue
@@ -1,113 +1,148 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <div class="wrap">
+        <div class="header">
+            <div class="head wraper">
+                <img src="../assets/logo.png" alt="">
+                <ul>
+                    <li>产品</li>
+                    <li>关于我们</li>
+                    <li>大数据</li>
+                    <li>新闻</li>
+                    <li>招聘</li>
+                    <li>商城</li>
+                    <li>登录/注册</li>
+                </ul>
+                <i class="icon"><img src="../assets/列表.png" alt=""></i>
+            </div>
+        </div>
+        <div class="banner">
+            <img src="../assets/banner.png" alt="">
+        </div>
+        <div class="tap">
+            <div class="taplist wraper">
+                <ul>
+                    <li>分析评论</li>
+                    <li>趋势榜单</li>
+                    <li>研究报告</li>
+                </ul>
+            </div>
+        </div>
+        <div class="section">
+            <div class="con wraper">
+                <div class="one">
+                    <h3><i></i><span>SUV</span><em>TOP10</em></h3>
+                    <div id="myChart" :style="{width: '500px', height: '400px'}"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  mounted(){
+    this.drawLine();
+  },
+  methods: {
+    drawLine(){
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = this.$echarts.init(document.getElementById('myChart'))
+        // 绘制图表
+        myChart.setOption({
+          tooltip: {
+              trigger: 'axis'
+          },
+          legend: {
+              data:['吉利博越','冠道','本田XR-V','哈弗H6','途观','全新途胜','本田CR-V','缤智','奔驰GLA级','汉兰达']
+          },
+          grid: {
+              left: '3%',
+              right: '4%',
+              bottom: '3%',
+              containLabel: true
+          },
+          toolbox: {
+              feature: {
+                  saveAsImage: {}
+              }
+          },
+          xAxis: {
+              type: 'category',
+              boundaryGap: false,
+              data: ['Jan/18','Mar/18','May/18','Jul/18','Sep/18','Nov/18']
+          },
+          yAxis: {
+              type: 'value'
+          },
+          series: [
+              {
+                  name:'吉利博越',
+                  type:'line',
+                  stack: '总量',
+                  data:[120, 132, 101, 134, 90, 230]
+              },
+              {
+                  name:'冠道',
+                  type:'line',
+                  stack: '总量',
+                  data:[220, 182, 191, 234, 290, 330]
+              },
+              {
+                  name:'本田XR-V',
+                  type:'line',
+                  stack: '总量',
+                  data:[150, 232, 201, 154, 190, 330]
+              },
+              {
+                  name:'哈弗H6',
+                  type:'line',
+                  stack: '总量',
+                  data:[320, 332, 301, 334, 390, 330]
+              },
+              {
+                  name:'全新途胜',
+                  type:'line',
+                  stack: '总量',
+                  data:[820, 932, 901, 934, 1290, 1330]
+              },
+              {
+                  name:'本田CR-V',
+                  type:'line',
+                  stack: '总量',
+                  data:[200, 300, 400, 500, 600, 700]
+              },
+              {
+                  name:'缤智',
+                  type:'line',
+                  stack: '总量',
+                  data:[123, 444, 785, 456, 789, 700]
+              },
+              {
+                  name:'奔驰GLA级',
+                  type:'line',
+                  stack: '总量',
+                  data:[155, 489, 357, 753, 851, 951]
+              },
+              {
+                  name:'汉兰达',
+                  type:'line',
+                  stack: '总量',
+                  data:[555, 666, 888, 772, 687, 786]
+              }
+          ]
+        });
     }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  @import '../common/style'
 </style>
--- day5/chelun/src/main.js
+++ day5/chelun/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import echarts from 'echarts'
+
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
